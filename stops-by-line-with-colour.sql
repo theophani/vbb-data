c09be11798af848686517fd96b9baf3d6bd6dd2b
Tiffany Conroy <tiffany.conroy@gmail.com>
2019-04-06 10:00:17 +0200

    Exclude S Bahns outside of Berlin-Brandenburg

--- stops-by-line-with-colour.sql
+++ stops-by-line-with-colour.sql
@@ -13,19 +13,20 @@ INNER JOIN (
 	INNER JOIN vbb_linienfarben
 		ON vbb_linienfarben.Linie = vbb_routes.route_short_name
 	WHERE (vbb_routes.route_short_name LIKE "S%" OR vbb_routes.route_short_name LIKE "U%")
+	AND agency_id != 108 # Exclude S Bahns outside of Berlin-Brandenburg
 ) as routes_with_colours
 	ON routes_with_colours.route_id = vbb_trips.route_id
 INNER JOIN vbb_stop_times
 	ON vbb_stop_times.trip_id = vbb_trips.trip_id
 INNER JOIN vbb_stops
 	ON vbb_stop_times.stop_id = vbb_stops.stop_id
-	
+
 WHERE direction_id = 0
 
 GROUP BY
 	routes_with_colours.route_short_name,
 	vbb_stops.stop_name
-	
+
 ORDER BY routes_with_colours.route_short_name
 
 ;
